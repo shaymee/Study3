b3a48aedde854c5f42abf3950c1630db3a391c4d
shaymee <acteasily@gmail.com>
2021-07-13 10:04:16 +0900

    Java3

--- src/com/google/s2/zz/Test8.java
+++ src/com/google/s2/zz/Test8.java
@@ -8,11 +8,13 @@ public class Test8 {
 		System.out.println("== Test8 Start ==");
 		
 		Scanner sc = new Scanner(System.in);
-		int kor = 0;
-		int eng = 0;
-		int math = 0;
-		int total = 0;
+//  ctrl+/ --> 전체 단일 주석, ctrl+/ --> 주석해제
+		int kor = 0; 
+		int eng = 0; 
+		int math = 0; 
+		int total = 0; 
 		double avg = 0;
+
 		System.out.println("kor 성적을 입력하세요");
 		kor = sc.nextInt();
 		System.out.println("eng 성적을 입력하세요");
@@ -50,9 +52,46 @@ public class Test8 {
 		
 		System.out.println(result); 
 			*/
+	
+//	 강사님 풀이1) 강사님 왈 "이 코드는 너무 복잡한게 단점"
+//	 if(avg>=60) {
+//		 if(kor>=40) {
+//			 if(eng>=40) {
+//				 if(math>=40) {
+//					 System.out.println("합격");		
+//				 } else {
+//					 System.out.println("수학 과락 불합격");
+//				 } else {
+//					 System.out.println("영어 과락 불합격");
+//				 } else {
+//					 System.out.println("국어 과락 불합격");
+//				 }
+//			 }
+//		 }
+//	 }  		
+
+//		강사님 풀이2) 논리연산자 && 활용
+				
+//		 if(avg>=60) {
+//			 if(kor>=40 && eng >=40 && math>=40) {  // 중간에 if-else문이 set
+//			 System.out.println("합격");
+//		 } else {
+//			 System.out.println("불합격");
+//		 } else {									// 맨 위, 맨 아래 if-else문이 set
+//			 System.out.println("불합격");
+//		 }
 		
+//		강사님 풀이3) &&을 활용해 더 간단히 코드를 표현
+
+		if(avg>=60 && kor>=40 && eng >=40 && math>=40) {
+			System.out.println("합격");
+		} else { 
+			System.out.println("불합격");
+		}	 
+			 
+			 
 		System.out.println("== Test8 End ==");
 
 	}
 
-}
+}	
